7217cf865c45aa282c160c3b355b7038f361dd39
Jacob Way <jacobbway1997@gmail.com>
2026-04-01 09:08:34 -0500

    Followed readme guide for readymail library

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,50 @@
 
 #define ENABLE_SMTP
 #define ENABLE_DEBUG
-#include <ReadMail.h>
+#include <ReadyMail.h>
 
-// put function declarations here:
-int myFunction(int, int);
+WiFiClientSecure ssl_client;
+SMTPClient smtp(ssl_client);
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  WiFi.begin("MY_SSID", "SSID_PASSWORD");
+  while (WiFi.status() != WL_CONNECTED) delay(500);
+
+  ssl_client.setInsecure();
+
+  auto statusCallback = [](SMTPStatus status) {
+    Serial.println(status.text);
+  };
+
+  smtp.connect("smtp.gmail.com", 465, statusCallback);
+
+  if(smtp.isConnected()) {
+    smtp.authenticate("wholecelerywaterdishemail@gmail.com", "its app password", readymail_auth_password);
+
+    SMTPMessage msg_Jacob;
+    msg_Jacob.headers.add(rfc822_from, "DogDish wholecelerywaterdishemail@gmail.com");
+    msg_Jacob.headers.add(rfc822_to, "Jacob 7159659801@tmomail.net");
+    msg_Jacob.headers.add(rfc822_subject, "Olive in Drought");
+    msg_Jacob.text.body("Olive needs water! She is dying *Palpatine Voice*");
+    msg_Jacob.html.body("<html><body><h1>Hello!</h1></body></html>");
+
+    SMTPMessage msg_Sophia;
+    msg_Sophia.headers.add(rfc822_from, "DogDish wholecelerywaterdishemail@gmail.com");
+    msg_Sophia.headers.add(rfc822_to, "Sophia 9125737@tmomail.net");
+    msg_Sophia.headers.add(rfc822_subject, "Olive in Drought");
+    msg_Sophia.text.body("Olive needs water! She is dying *Palpatine Voice*");
+    msg_Sophia.html.body("<html><body><h1>Hello!</h1></body></html>");
+
+    configTime(0, 0, "pool.ntp.org");
+    while(time(nullptr) < 100000) delay(100);
+    msg_Jacob.timestamp = time(nullptr);
+    msg_Sophia.timestamp= time(nullptr);
+
+    smtp.send(msg_Jacob);
+    smtp.send(msg_Sophia);
+  }
 }
 
 void loop() {
