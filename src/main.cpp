cbc48288241e4828912050b527845db28acb73c4
Jacob Way <jacobbway1997@gmail.com>
2026-04-11 15:39:18 -0500

    hx711

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <Arduino.h>
 
+// 1. HX711 circuit wiring
+const int LOADCELL_DOUT_PIN = 2;
+const int LOADCELL_SCK_PIN = 3;
+
+// 2. Adjustment settings
+const long LOADCELL_OFFSET = 50682624;
+const long LOADCEL_DIVDER = 5895655;
+
 void setup() {
 
 }
