6b3f9f0fcaead9a88ad072732ef30ad1382bed90
Jacob Way <jacobbway1997@gmail.com>
2026-04-14 09:58:05 -0500

    reading works

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include "HX711_ADC.h"
+
+#if defined(ESP8266)|| defined(ESP32) || defined(AVR)
 #include <EEPROM.h>
+#endif
 
 const int HX711_dout = 46; // HX711 d out pin
 const int HX711_sck = 16; // HX711 sck pin
@@ -13,25 +16,38 @@ void HX711Setup(); // function declaration
 
 void setup(){
   Serial.begin(57600);
+  LoadCell.begin();
   HX711Setup();
-  
 }
 
 void loop(){
-  delay(2000);
-  LoadCell.powerUp();
-  if(LoadCell.update() < lowerBound){
-    Serial.println("\n\n\nWater Dish is Empty!\n");
+  static boolean newDataReady = 0;
+  const int serialPrintInterval = 5000; //increase value to slow down serial print activity
+
+  // check for new data/start next conversion:
+  if (LoadCell.update()) newDataReady = true;
+
+  // get smoothed value from the dataset:
+  if (newDataReady) {
+    if (millis() > t + serialPrintInterval) {
+      float i = LoadCell.getData();
+      Serial.print("Load_cell output val: ");
+      Serial.println(i);
+      if(i < lowerBound){
+        Serial.println("Water Dog");
+      }
+      newDataReady = 0;
+      t = millis();
+    }
   }
-  LoadCell.powerDown();
 }
 
 
 // 62 grams
 void HX711Setup() {
-  Serial.println("\n\n\n");
+  Serial.println("\n");
   Serial.println("Starting HX711...");
-  LoadCell.begin();
+
   float calibrationValue = 763.17; // 763.17 = calibration factor without plate on it
 
   unsigned long stabilizingTime = 2000; // lets the precision improve by allowing more time for stabilizing
